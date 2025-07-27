3fc8215f40d37299897f0272298f991ff376a526
akkupratap323 <151503296+akkupratap323@users.noreply.github.com>
2025-07-27 13:21:36 +0530

    final touch

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
   },
+  output: 'standalone',
+  poweredByHeader: false,
+  compress: true,
 }
 
 module.exports = nextConfig
